c17b92f71153494c1b1eb579d74eb924c9f8b925
ZEK-XCPY <zaid03297@gmail.com>
2026-01-24 20:55:00 +0530

    Sign Up a user function

---
+++ server/controllers/userController.js
@@ -0,0 +1,38 @@
+// function create user , to login , to authenticate ,update userProfile
+
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+
+
+// SignUp a New user
+export const signup = async ()=>{
+  const {fullName,email,password,bio} = req.body;
+
+  try {
+      if(!fullName || !email || !password || !bio){
+        return res.json({success:false, message:"Missing Details"})
+      }
+      const user = await User.findOne({email})
+      if(user){
+        return res.json({success:false, message:"Account already exists"})
+      }
+
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(password,salt);
+
+      const newUser = await User.create({
+        fullName,email,password:hashedPassword,bio
+      });
+
+      const token = generateToken(newUser._id)
+
+      res.json({success:true, userData: newUser, token , message:"Account created successfully"})
+
+  } catch (error) {
+    console.log(error.message)
+    res.json({success:false,  message:error.message})
+  }
+}
+
+// 
