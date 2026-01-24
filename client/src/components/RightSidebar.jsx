377fbc481d88ff490545786325153d4f6d763896
ZEK-XCPY <zaid03297@gmail.com>
2026-01-24 11:03:18 +0530

    Add Sidebar chat section

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { imagesDummyData } from '../assets/assets'
 
 const RightSidebar = ({selectedUser}) => {
   return selectedUser &&  (
-    <div className={``}> 
+    <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ""} `}> 
+
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
           <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" 
            className='w-20 aspect-square rounded-full'/>
@@ -13,6 +14,18 @@ const RightSidebar = ({selectedUser}) => {
            </h1>
            <p className='px-10 mx-auto'>{selectedUser.bio}</p>
       </div>
+        <hr className='border-[#ffffff50] my-4'/>
+        <div className='ppx-5 text-xs'>
+              <p>Media</p>
+              <div className='mt-2 max-h-50 overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
+                  {imagesDummyData.map((url,index)=>(
+                    <div key={index} onClick={()=>window.open(url)} className='cursor-pointer rounded'>
+                          <img src={url} alt=""  className='h-full rounded-md'/>
+                    </div>
+                  ))}
+              </div>
+        </div>
+        
       </div>
   )
 }
